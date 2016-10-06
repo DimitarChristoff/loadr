4fe3f07516fa8d88761d1ce9778b617b00121f77
DimitarChristoff <dimitar.christoff1@rbs.com>
2016-10-06 10:20:20 +0100

    tweaks to nodejs example

--- example/nodetest.js
+++ example/nodetest.js
@@ -1,12 +1,17 @@
 #!/bin/env node
+'use strict';
 const Loadr               = require('..').default;
 const stdin               = process.stdin;
 const stdout              = process.stdout;
 const colors              = require('colors');
 const dance               = [' o'.yellow + '/'.red + '< '.cyan, ' o'.yellow + '\\'.red + '< '.cyan];
+const cols                = ['green', 'red'];
+const spinner             = '⠁⠂⠄⡀⢀⠠⠐⠈'.split('');
+
+let i = 0;
 const instance            = new Loadr(null, {
   after: '...'.green,
-  before: dance[0]
+  before: ' ' + spinner[i] + ' '
 });
 
 // without this, we would only get streams once enter is pressed
@@ -20,13 +25,19 @@ function getMessage(){
   stdout.clearLine();  // clear current text
   stdout.cursorTo(0);
   stdout.write(instance.get());
-  instance.options.before = instance.options.before === dance[0] ? dance[1] : dance[0];
+  i = i < spinner.length-1 ? i+1 : 0;
+  instance.options.before = ' ' + spinner[i][cols[~~(Math.random() * cols.length)]] + ' ';
   setTimeout(getMessage, 1000);
 }
 
+console.log('');
+console.log('   _  ___'+'.oOo.'.yellow+'____'+'loadr'.green+'____'+'.oOo.'.yellow+'_______________________ ____  __    _  .');
+console.log('');
 getMessage();
 
 stdin.on('data', function(){
+  stdout.clearLine();  // clear current text
+  stdout.cursorTo(0);
+  console.log('Bye.'.red);
   process.exit();
-  console.log('Bye');
 });
